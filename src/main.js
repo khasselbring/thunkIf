7afa33a4b8b4f442cc968b0b1c243f7079c5ad09
kalleaush <karl.hasselbring@gmx.de>
2017-04-17 14:59:18 +0200

    Added Runtime Behavior tests

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import * as Graph from '@buggyorg/graphtools'
 
-function times(n, f) {
-  return function() {
+export function times (n, f) {
+  return function () {
     for (var i = 0; i < n; i++) {
       f(arguments)
     }
   }
 }
 
-function timesIntermediate(n, f) {
-  return function(x) {
+export function timesIntermediate (n, f) {
+  return function (x) {
     for (var i = 0; i < n; i++) {
       x = f(x)
     }
@@ -18,20 +18,72 @@ function timesIntermediate(n, f) {
 }
 
 /**
+ * Checks whether values a and b differ by a maximum of epsilon percent of the higher of both values
+ * @param {number} a first value
+ * @param {number} b second value
+ * @param {number} epsilon maximum differal percentage
+ */
+export function inEpsilonRange (a, b, epsilon) {
+  return Math.abs(a - b) <= epsilon * Math.max(a, b) * 0.01
+}
+
+/**
+ * TODO
+ * @param {number[]} values 
+ * @param {string} behavior 
+ */
+export function hasGrowthBehavior (values, behavior, epsilon) {
+  var f
+  switch (behavior) {
+    case 'linear':
+      f = function (last, next) {
+        return next - last
+      }
+      break
+    case 'exponential':
+      f = function (last, next) {
+        return next / last
+      }
+      break
+    default:
+      f = function (last, next) {
+        return 0
+      }
+  }
+  return growsByFunction(values, f, epsilon)
+}
+
+/**
+ * Checks whether all values grow in a given behavior.
+ * The behavior function receives two consecutive values and normalizes them
+ * @param {*} values the values to check
+ * @param {*} f the growth function
+ * @param {*} epsilon a maximum differal between observed and expected results
+ */
+export function growsByFunction (values, f, epsilon) {
+  var last = f(values[0], values[1])
+  for (var i = 2; i < values.length; i++) {
+    var next = f(values[i - 1], values[i])
+    if (!inEpsilonRange(next, last, epsilon)) return false
+    last = next
+  }
+  return true
+}
+
+/**
  * Runs a function and measures the time needed to do so
- * 
  * @param {function} f The function to run
  * @returns {milliseconds} The time used by the function
  */
-exports.executionTime = function (f) {
+export function executionTime (f) {
   var before = new Date()
   f()
   var after = new Date()
   return after - before
 }
 
-export default function addNodeAmount(amount) {
-  var graph = timesIntermediate(amount, function(graph) {
+export function addNodeAmount (amount) {
+  var graph = timesIntermediate(amount, function (graph) {
     return Graph.flow(
       Graph.addNode({})
     )(graph)
--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 import * as Graph from '@buggyorg/graphtools'
-import addNodeAmount from '../src/main'
+import * as Runtime from '../src/main'
 import chai from 'chai'
-import fs from 'fs'
 
 const expect = chai.expect
 
 describe('» Playground', () => {
   it('can add Nodes', () => {
     const graph = Graph.flow(
-      Graph.addNode({name: 'a', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
-      Graph.addNode({name: 'b', ports: [{port: 'in', kind: 'input', type: 'Number'}]})
+      Graph.addNode({ name: 'a', ports: [{ port: 'out', kind: 'output', type: 'Number' }] }),
+      Graph.addNode({ name: 'b', ports: [{ port: 'in', kind: 'input', type: 'Number' }] })
     )()
 
     expect(Graph.hasNode('a', graph)).to.be.true
@@ -18,15 +17,25 @@ describe('» Playground', () => {
 
   it('can add Edges', function () {
     const graph = Graph.flow(
-      Graph.addNode({name: 'a', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
-      Graph.addNode({name: 'b', ports: [{port: 'in', kind: 'input', type: 'Number'}]}),
-      Graph.addEdge({name: 'a-to-b', from: 'a@out', to: 'b@in'})
+      Graph.addNode({ name: 'a', ports: [{ port: 'out', kind: 'output', type: 'Number' }] }),
+      Graph.addNode({ name: 'b', ports: [{ port: 'in', kind: 'input', type: 'Number' }] })
     )()
 
     expect(Graph.hasNode('a', graph)).to.be.true
     expect(Graph.hasNode('b', graph)).to.be.true
-    expect(Graph.edges(graph)).to.have.length(1)
-    expect(Graph.areConnected('a', 'b', graph)).to.be.true
+    expect(Graph.edges(graph)).to.have.length(0)
+    expect(Graph.areConnected('a', 'b', graph)).to.be.false
+
+    const graph2 = Graph.addEdge({ name: 'a-to-b', from: 'a@out', to: 'b@in' }, graph)
+
+    expect(Graph.edges(graph2)).to.have.length(1)
+    expect(Graph.areConnected('a', 'b', graph2)).to.be.true
+
+    const edge = Graph.Edge.normalize({ from: 'a@out', to: 'b@in' })
+    const graph3 = Graph.addEdge(edge, graph)
+
+    expect(Graph.edges(graph3)).to.have.length(1)
+    expect(Graph.areConnected('a', 'b', graph3)).to.be.true
   })
 })
 
@@ -35,23 +44,49 @@ describe('» Runtime', () => {
     const size = 10
 
     var before = new Date()
-    var graph = addNodeAmount(size)
+    var graph = Runtime.addNodeAmount(size)
     var after = new Date()
-    var diff = after - before;
+    var diff = after - before
 
     expect(Graph.nodes(graph)).to.have.length(size)
     expect(diff).to.be.above(0)
   })
 
   it('increases runtime when adding more nodes', () => {
-    var main = require('../src/main.js')
-    var oldTime = 0;
-    for(var i = 10; i <= 100; i += 10) {
-      var newTime = main.executionTime(function() {
-        addNodeAmount(i)
-      })
-      expect(newTime - oldTime).to.be.above(0)
-      oldTime = newTime
+    var timings = []
+
+    for (var i = 10; i <= 200; i += 10) {
+      timings.push(Runtime.executionTime(function () {
+        Runtime.addNodeAmount(i)
+      }))
     }
+
+    expect(true).to.be.true
+  })
+})
+
+describe('» Behavior', () => {
+  it('Correctly detects linear Behavior', () => {
+    const values = [0, 1, 2, 3, 4, 5, 6]
+
+    expect(Runtime.hasGrowthBehavior(values, 'linear', 10)).to.be.true
+  })
+
+  it('Correctly detects missing linear Behavior', () => {
+    const values = [0, 1, 2, 7, 4, 5, 6]
+
+    expect(Runtime.hasGrowthBehavior(values, 'linear', 10)).to.be.false
+  })
+
+  it('Correctly detects exponential Behavior', () => {
+    const values = [1, 2, 4, 8, 16, 32]
+
+    expect(Runtime.hasGrowthBehavior(values, 'exponential', 10)).to.be.true
+  })
+
+  it('Correctly detects missing exponential Behavior', () => {
+    const values = [1, 2, 4, 5, 16, 32]
+
+    expect(Runtime.hasGrowthBehavior(values, 'exponential', 10)).to.be.false
   })
 })
